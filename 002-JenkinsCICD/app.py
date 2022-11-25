f9da32f7a3cf62b926f9b2b38148239a864d888d
zsb8 <songbin.zhang@gmail.com>
2022-11-25 12:14:32 -0500

    test

--- 002-JenkinsCICD/app.py
+++ 002-JenkinsCICD/app.py
@@ -1,9 +1,9 @@
-from flask import Flask
-app = Flask(__name__)
+fromflaskimportFlask
+app=Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!zsb2222222"
+defhello():
+return"HelloWorld!zsb2222222"
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+if__name__=="__main__":
+app.run(host='0.0.0.0',port=8080)
